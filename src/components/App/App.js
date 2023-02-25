87d43b28795f89edfd9898e6726f3165cd99feb4
Mike Goren <mike@mortalwombat.net>
2023-02-24 17:08:42 -0800

    prep project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from '../Header';
+import Form from '../Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="App-header">
+      <p><code>Hello, world</code></p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">an exciting link</a>
+    </header>
+  );
+}
