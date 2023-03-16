ceb2439c4dc51387314b25e3a56a6da4503b7b62
Mike Goren <mike@mortalwombat.net>
2023-03-16 14:09:57 -0700

    refactor Input again to use renderContent

--- src/components/MainForm/Input/Input.js
+++ src/components/MainForm/Input/Input.js
@@ -4,12 +4,13 @@ import { PatternFormat } from 'react-number-format';
 import * as S from '../Form-styles';
 
 const Input = ({ label, pattern, ...props }) => {
+  const { name } = props;
   const [field, meta] = useField(props);
   return (
     <div className='form-group'>
       <div className='row'>
         <div className="form-group col-sm-8">
-          <S.Label className='S.Label' htmlFor={props.id || props.name}>{label}</S.Label>
+          <S.Label className='S.Label' htmlFor={name}>{label}</S.Label>
           {!pattern && <input className='form-control' {...field} {...props} />}
           {pattern && <PatternFormat className='form-control' format={pattern} mask="_" {...field} {...props} />}
           <S.Warning className={`S.Warning ${meta.touched && meta.error ? 'visible' : ''}`}>{meta.error || '-'}</S.Warning>
@@ -19,24 +20,50 @@ const Input = ({ label, pattern, ...props }) => {
   );
 };
 
-const NumericInput = (props) => {
+const NumericInput = ({ label, range, showDollarSign, ...props }) => {
+  const { name, onBlur } = props;
+  const [field] = useField(props);
   return (
-    <RightAlignedInput {...props} >
-      <NumericInputContent {...props} />
-    </RightAlignedInput>
+    <RightAlignedInput name={name} label={label} renderContent={() => {
+      return (
+        <>
+          {showDollarSign && <div className="input-group-prepend"><span className="input-group-text" style={{padding: 6}}>$</span></div>}
+          <input 
+            type={isMobile ? 'tel' : 'number'} 
+            className={`numericInput ${isMobile ? "form-control" : "text-center"}`} 
+            {...field} 
+            name={name} 
+            id={name} 
+            onBlur={onBlur}
+            min={range[0]} max={range[1]} step="1" 
+          />
+        </>
+      );
+    }} />
+  );
+};
+
+const ButtonInput = ({ label, buttonText, ...props }) => {
+  const { name, onClick } = props;
+  const [field] = useField(props);
+  return (
+    <RightAlignedInput name={name} label={label} renderContent={() => {
+      return (
+        <S.DonationButton 
+          type='button' 
+          className='btn btn-sm btn-secondary' 
+          {...field} 
+          onClick={onClick} 
+        >
+          {buttonText}
+        </S.DonationButton>
+      );
+    }}/>
   );
 }
 
-const ButtonInput = (props) => {
-  return (
-    <RightAlignedInput {...props} >
-      <ButtonInputContent {...props} />
-    </RightAlignedInput>
-  );
-}
 
-const RightAlignedInput = (props) => {
-  const { name, label, children } = props;
+const RightAlignedInput = ({ name, label, renderContent }) => {
   return (
     <div className='form-group'>
       <div className={isMobile ? '' : 'row'}>
@@ -45,38 +72,12 @@ const RightAlignedInput = (props) => {
         </div>
         <div className="col-sm-3">
           <div className={`input-group mb-3 ${isMobile ? '' : 'justify-content-end'}`}>
-            { children }
+            { renderContent() }
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-const NumericInputContent = (props) => {
-  const { name, range, showDollarSign } = props;
-  const [field] = useField(props);
-  return (
-    <>
-      {showDollarSign && <div className="input-group-prepend"><span className="input-group-text" style={{padding: 6}}>$</span></div>}
-      <input 
-        type={isMobile ? 'tel' : 'number'} 
-        className={`numericInput ${isMobile ? "form-control" : "text-center"}`} 
-        {...field} name={name} id={name} onBlur={props.onBlur}
-        min={range[0]} max={range[1]} step="1" 
-      />
-    </>
-  );
-};
-
-const ButtonInputContent = (props) => {
-  const { buttonText, onClick } = props;
-  const [field] = useField(props);
-  return (
-    <S.DonationButton type='button' className='btn btn-sm btn-secondary' {...field} onClick={onClick} >
-      {buttonText}
-    </S.DonationButton>
-  );
-}
 
 export { Input, NumericInput, ButtonInput };
