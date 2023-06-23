4746b80f793b0b38293180d40713dabcbd7dd8cf
Mike Goren <mike@epicodus.com>
2023-06-23 01:21:02 -0700

    update intro header

--- src/components/Header/IntroHeader/IntroHeader.js
+++ src/components/Header/IntroHeader/IntroHeader.js
@@ -1,13 +1,11 @@
-import { COVID_POLICY_URL, EMAIL_CONTACT } from "config";
-import { mailtoLink, websiteLink } from "utils";
+import { EMAIL_CONTACT } from "config";
+import { mailtoLink } from "utils";
 import { Typography } from '@mui/material';
 import { StyledLink } from 'components/Layout/SharedStyles';
 
 export default function IntroHeader() {
   return (
     <>
-      <Typography gutterBottom={true}>Here is some information about the dance.</Typography>
-      <Typography gutterBottom={true}><strong>Covid policy:</strong> Summary here. PCDC's full Covid policy is available <StyledLink to={websiteLink(COVID_POLICY_URL)}>here</StyledLink>.</Typography>
       <Typography gutterBottom={true}>Questions? Email <StyledLink to={mailtoLink(EMAIL_CONTACT)}>{EMAIL_CONTACT}</StyledLink>.</Typography>
     </>
   );
